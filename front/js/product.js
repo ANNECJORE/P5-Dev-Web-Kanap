6f49968090d31b1f996b0dbae32c401a64a5c269
Annechristine Joré <annecjore@gmail.com>
2022-08-01 13:19:17 +0200

    vlundi

--- front/js/product.js
+++ front/js/product.js
@@ -1,27 +1,25 @@
 const str = window.location.href;
 const url = new URL(str);
 const productId = url.searchParams.get("id");
-  
-const prix = document.querySelector('#price') 
-const couleur =document.querySelector('#colors')
-const nom = document.querySelector('#title')
-const descript = document.querySelector('#description')
-const zoneimage = 
+const prix = document.querySelector("#price");
+const couleur = document.querySelector("#colors");
+const nom = document.querySelector("#title");
+const descript = document.getElementById("description");
+//const zoneimage = document.querySelector(".item__img");
 
-fetch('http://localhost:3000/api/products/'+ productId) 
-    .then (response => response.json())
-   .then(async function (produit)
- 
-     {
-    prix.innerHTML=produit.price;
+fetch("http://localhost:3000/api/products/" + productId)
+  .then((response) => response.json())
+  .then(async function (produit) {
+    nom.innerText = produit.name;
+    //console.log(produit.name);
+
+    prix.innerHTML = produit.price;
     descript.innerHTML = produit.description;
-    produit.colors.forEach(color => {
-    let option = document.createElement ("option");
-    option.value = color;
-    option.text = color;
-   couleur.appendChild(option);
+    produit.colors.forEach((color) => {
+      let option = document.createElement("option");
+      option.value = color;
+      option.text = color;
+      couleur.appendChild(option);
     });
-    console.log(produit.colors);   
-}) 
- 
-
+    console.log(produit.colors);
+  });
