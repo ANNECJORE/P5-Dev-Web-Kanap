9cab05246ef94f73239358cbcc615b93e2b08885
Annechristine Joré <annecjore@gmail.com>
2022-08-22 07:52:08 +0200

    eafficher

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,118 +1,114 @@
-// const contenu = document.getElementbyId("cart__item");
-fetch("http://localhost:3000/api/products")
-  .then((response) => response.json())
-  .then(async function (recup) {
-    catalogCanape = await recup;
-    console.log(recup);
-    allCatalogCanape(catalogCanape);
-  });
-
-// function contenuPanier() {
-//   let choixPanier = [];
-//   if (localStorage.getItem("selection")  === 0) {
-//     alert('votre panier est vide');
-
-//     if choixPanier = JSON.parse(localStorage.getItem("selection"));
-
-//     console.log(choixPanier);
-//   }
-//   // alert("coucou");
-// }
-
 let contenuPanier = JSON.parse(localStorage.getItem("selection"));
 //panier vide
-if ((contenuPanier == 0, (contenuPanier.length = 0))) {
-  document.querySelector("h1").innerHTML += "votre panier est vide";
+if (contenuPanier.length === 0) {
+  document.querySelector("h1").innerHTML += " est vide";
 } else {
-  document.querySelector("h1").innerHTML += "votre panier";
-}
-// récuperer article
-function articleEssai(produit) {
-  const article = document.creatElement("article");
-  article.classList.add("cart__item");
-  article.dataset.id = produit.id;
-  article.datacolor.id = produit.color;
-  article.appendChild("cart__item");
-  return article;
+  // document.querySelector("h1").innerHTML = "Votre panier ";
+
+  panierAfficher();
+  panierTotal();
 }
 
-document.getElementsByClassName("cart__items").appendChild(article);
-
-//créer l'image
-function imageEssai(choixPanier) {
-  const div = document.creatElement("div");
-  div.classList.add("art__item__img");
-  image.appendChild(article);
-  image.src = choixPanier.imageUrl;
-  image.alt = choixPanier.altTxt;
-  const image = document.creatElement("img");
-  return image;
-}
-const contentImage = document.creatElement("cart__content__img");
-
-function panierChoisi(div, detailCanape) {
-  const detailCanape = document.creatElement("div");
-  detailCanape.classList.add("cart__item__content__description");
-
-  const nomCanape = document.creatElement("h2");
-  nomCanape.textContent = produit.name;
-  const couleurChoix = document.creatElement("p");
-  couleurChoix.textContent = produit.colors;
-  const prixTotal = document.creatElement("p");
-  prixTotal.textContent = produit.price + " €";
-
-  detailCanape.appendChild(nomCanape);
-  detailCanape.appendChild(couleurChoix);
-  detailCanape.appendChild(prixTotal);
-  div.appendChild(detailCanape);
-}
-
-prixTotal.appendChild("h2");
-
-// nombre de canapés
-
-const combienCanape = document.creatElement("div");
-combienCanape.classList.add("cart__item__content__settings__quantity");
-const totalChoix = document.creatElement("p");
-totalChoix.textContent = "Qté : ";
-combienCanape.appendChild(totalChoix);
-
-nbCanape.textContent = produit.combienCanape;
-
-const input = document.createElement("input");
-combienCanape.appendChild(input);
-
-// bouton
-input.type = "number";
-input.classList.add("itemQuantity");
-input.name = itemQuantity;
-input.value = produit.combienCanape;
-input.min = 1;
-input.max = 100;
-
-// enlever des produits
-function onRetireTrucs() {
-  const retirer = document.creatElement("div");
-  retirer.classList.add("cart__item__content__settings__delete");
-  const oter = document.createElement("p");
-  oter.textContent = "supprimer";
-  retirer.appendChild("oter");
-}
-
-// function panierRempli(produit) {
-//   const nbCanape = document.getElementById("totalQuantity");
-
-//   const panierVide = produit[0];
-//   const panierPlein = panierVide.combienCanape * panierVide.price;
-// }
-
-// prix total
-function panierRempli() {
-  let total = 0;
+// calcul et affichage du prix total
+function panierTotal() {
+  let prixTotal = 0;
+  let qteTotal = 0;
   const factureTotal = document.getElementById("totalPrice");
-  cart.forEach((produit) => {
-    const prixTotal = produit.price * produit.combienCanape;
-    prixTotal += total;
-    factureTotal.textContent = prixTotal;
+  const articleTotal = document.getElementById("totalQuantity");
+  contenuPanier.forEach((produit) => {
+    fetch("http://localhost:3000/api/products/" + produit.id)
+      .then((response) => response.json())
+      .then(async function (recupInfoProduit) {
+        recupProduit = await recupInfoProduit;
+        console.log(recupProduit);
+      });
+    prixTotal += recupProduit.price * produit.quantite;
+    qteTotal += produit.quantite;
+    factureTotal.textContent = prixtotal;
+    articleTotal.textContent = qteTotal;
   });
 }
+
+function panierAfficher() {
+  const contenu = document.querySelector("#cart__items");
+  contenuPanier.forEach((produit) => {
+    let recupProduit;
+    // récupération des information du produit dans l'API
+    fetch("http://localhost:3000/api/products/" + produit.id)
+      .then((response) => response.json())
+      .then(async function (recupInfoProduit) {
+        recupProduit = await recupInfoProduit;
+
+        console.log(recupProduit);
+        // création de l'article qui contient toutes les informations du produit
+        const article = document.createElement("article");
+        article.className = "cart__item";
+        article.dataset.id = produit.id;
+        article.dataset.color = produit.couleur;
+
+        // création de la div qui contient l'image
+        const divImg = document.createElement("div");
+        divImg.className.add("art__item__img");
+        // création de l'image
+        const image = document.createElement("img");
+        image.src = recupProduit.imageUrl;
+        image.alt = recupProduit.altTxt;
+
+        // création de la div qui contient les info produit
+        const panierContenu = document.createElement("div");
+        panierContenu.className = "cart__item__content";
+
+        // création de la div qui contient la description du produit
+        const detailCanape = document.createElement("div");
+        detailCanape.className.add("cart__item__content__description");
+
+        // elément de la description produit
+        const nomCanape = document.createElement("h2");
+        nomCanape.textContent = recupProduit.name;
+        const couleurChoix = document.createElement("p");
+        couleurChoix.textContent = produit.couleur;
+        const prix = document.createElement("p");
+        prix.textContent = recupProduit.price + " €";
+
+        // création de la div qui contient la quatité et les boutons d'action
+        const divSetting = document.createElement("div");
+        divSetting.className.add("cart__item__content__settings");
+
+        // création de la quatité
+        const combienCanape = document.createElement("div");
+        combienCanape.className.add("cart__item__content__settings__quantity");
+        const totalChoix = document.createElement("p");
+        totalChoix.textContent = "Qté : ";
+        const input = document.createElement("input");
+        input.type = "number";
+        input.classList.add("itemQuantity");
+        input.name = "itemQuantity";
+        input.value = produit.quantite;
+        input.min = 1;
+        input.max = 100;
+
+        const retirer = document.createElement("div");
+        retirer.className.add("cart__item__content__settings__delete");
+        const oter = document.createElement("p");
+        oter.textContent = "supprimer";
+
+        //Ajout des éléments dans l'article
+        article.appendChild(divImg);
+        divImg.appendChild(image);
+        article.appendChild(panierContenu);
+        panierContenu.appendChild(detailCanape);
+        detailCanape.appendChild(nomCanape);
+        detailCanape.appendChild(couleurChoix);
+        detailCanape.appendChild(prix);
+        panierContenu.appendChild(divSetting);
+        divSetting.appendChild(combienCanape);
+        combienCanape.appendChild(totalChoix);
+        combienCanape.appendChild(input);
+        divSetting.appendChild(retirer);
+        retirer.appendChild(oter);
+        contenu.appendChild(article);
+      });
+  });
+}
+
+function onRetireTrucs() {}
